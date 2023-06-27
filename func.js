0ba2122a3db650f2b626ada928c51522156ef86f
benjaminGuirin <126121809+benjaminGuirin@users.noreply.github.com>
2023-06-27 19:18:37 -0300

    page

---
+++ func.js
@@ -0,0 +1,60 @@
+//-----------------------------menu--------------------------//
+let navbar = document.querySelector('.navbar');
+
+
+document.querySelector('#menu-btn').onclick = ()=>{
+    navbar.classList.toggle('active');
+}
+ //optenemos los enlaces del navbar
+ const enlaces = navbar.querySelectorAll('a');
+ //agregamos el controlador
+ enlaces.forEach(function(enlace){
+     enlace.addEventListener('click',function(){
+         navbar.classList.remove('active');
+     })
+
+ });
+
+
+ Window.onscroll = ()=>{
+     navbar.classList.remove('active');
+ }
+//-------------------------------mandar email------------------------------------//
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_2a0lcc5';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Contactanos';
+      alert('Mensaje enviado correctamente');
+    }, (err) => {
+      btn.value = 'Contactanos';
+      alert(JSON.stringify(err));
+    });
+});
+
+// //----------------------Efectos de scroll------------------------//
+
+const seccionesOcultas = document.querySelectorAll('.oculto');
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(( entry)=> {
+        entry.target.classList.toggle('mostrar',entry.isIntersecting);
+    })
+})
+
+seccionesOcultas.forEach((seccion)=>observer.observe(seccion));
+
+
+
+
+
+
